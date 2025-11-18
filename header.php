d27c7a5234ef20bb432195b1e8a284f0ffbd3ca5
Pradipdan <97512832+Pradipdan@users.noreply.github.com>
2025-11-18 13:16:26 +0530

    Include header and footer in index.php

---
+++ header.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <title></title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon" />
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect" />
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet" />
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet" />
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+
+    <!-- Main CSS File -->
+    <link href="assets/css/main.css" rel="stylesheet" />
+</head>
+
+<body class="index-page">
+    <header class="container-fluid nopadding">
+        <div class="headbg">
+            <div class="">
+                <div class="m-auto">
+                    <div class="container">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 centerall">
+                            <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 logo">
+                                <a href="/">
+                                    <!--<img src="http://brendaarnoldscott.com/newsite/wp-content/uploads/2018/01/logos-new.png" alt="logo">-->
+                                    <img src="/assets/img/logo.svg" alt="logos" />
+                                </a>
+                            </div>
+
+                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 topspaces">
+                                <a href="/">
+                                    <img src="assets/img/logos.png" alt="logo" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <nav id="navmenu" class="navmenu centerall">
+                        <ul>
+                            <li><a href="#hero">Home</a></li>
+                            <li class="dropdown">
+                                <a href="#"><span>About</span>
+                                    <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                                <ul>
+                                    <li><a href="#">Bio</a></li>
+
+                                    <li><a href="#">Ministry</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#"><span>Products</span>
+                                    <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                                <ul>
+                                    <li><a href="#">Books</a></li>
+
+                                    <li><a href="#">CDs</a></li>
+                                    <li><a href="#">Poetry Prints</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#services">E Store</a></li>
+
+                            <li class="dropdown">
+                                <a href="#"><span>Connect</span>
+                                    <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                                <ul>
+                                    <li><a href="#">Contact Us</a></li>
+
+                                    <li><a href="#">Online Giving</a></li>
+                                    <li><a href="#">Prayer request</a></li>
+                                    <li><a href="#">Gallery</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -1,5 +1,2 @@
 <?php include 'header.php'; ?>
-
-<!-- Main content goes here -->
-
 <?php include 'footer.php'; ?>
